ccb3e2a07a282664184bba4daff385dc420824b0
MaciekG98 <49412535+MaciekG98@users.noreply.github.com>
2020-03-16 11:52:46 +0100

    z zadan

---
+++ Cwiczenia_03/Cwiczenia_03/Controllers/StudentsController.cs
@@ -0,0 +1,36 @@
+﻿using Cwiczenia_03.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenia_03.Controllers
+{
+   
+   
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController: ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents(string orderBy)
+        {
+            return $"Jan, Anna, Katarzyna posortowane ={orderBy}";
+        }
+
+/*        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+
+
+            return NotFound("NIE ZNALEZIONO STUDENT");
+        }*/
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+    }
+}
