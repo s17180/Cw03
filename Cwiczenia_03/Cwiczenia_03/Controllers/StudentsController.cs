1e03abd09d5e322ef6ccfc044dad55751687fef4
MaciekG98 <49412535+MaciekG98@users.noreply.github.com>
2020-03-21 16:27:37 +0100

    DONE

--- Cwiczenia_03/Cwiczenia_03/Controllers/StudentsController.cs
+++ Cwiczenia_03/Cwiczenia_03/Controllers/StudentsController.cs
@@ -1,36 +1,45 @@
-﻿using Cwiczenia_03.Models;
+﻿using Cwiczenia_03.DAL;
+using Cwiczenia_03.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Cwiczenia_03.Controllers
 {
-   
    
     [ApiController]
     [Route("api/students")]
     public class StudentsController: ControllerBase
     {
-        [HttpGet]
-        public string GetStudents(string orderBy)
+
+        private readonly IDbService _dbService;
+        
+        public StudentsController(IDbService dbService)
         {
-            return $"Jan, Anna, Katarzyna posortowane ={orderBy}";
+            _dbService = dbService;
         }
 
-/*        [HttpGet("{id}")]
-        public IActionResult GetStudent(int id)
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
         {
+            return Ok(_dbService.GetStudents());
+        }
 
-
-            return NotFound("NIE ZNALEZIONO STUDENT");
-        }*/
+      
         [HttpPost]
         public IActionResult CreateStudent(Student student)
         {
             student.IndexNumber = $"s{new Random().Next(1, 20000)}";
             return Ok(student);
+        }
+        [HttpPut("{id}")]
+        public IActionResult putStudent(int id)
+        {
+            return Ok("Update succeed " + id);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult deleteStudent(int id)
+        {
+            return Ok("Delete succeed " + id);
         }
     }
 }
