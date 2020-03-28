bd0020f163970c2fc3c8e62b668e1ff6547b272e
MaciekG98 <49412535+MaciekG98@users.noreply.github.com>
2020-03-28 14:43:09 +0100

    PRACA DOMOWA Z CW04

--- Cwiczenia_03/Cwiczenia_03/Controllers/StudentsController.cs
+++ Cwiczenia_03/Cwiczenia_03/Controllers/StudentsController.cs
@@ -7,14 +7,14 @@ using System.Data.SqlClient;
 
 namespace Cwiczenia_03.Controllers
 {
-   
+
     [ApiController]
     [Route("api/students")]
-    public class StudentsController: ControllerBase
+    public class StudentsController : ControllerBase
     {
 
         private readonly IDbService _dbService;
-        
+
         public StudentsController(IDbService dbService)
         {
             _dbService = dbService;
@@ -25,7 +25,7 @@ namespace Cwiczenia_03.Controllers
         {
             string conString = "Data Source = db-mssql; Initial Catalog = s17180; Integrated Security = True";
             using (SqlConnection con = new SqlConnection(conString))
-            using(SqlCommand com = new SqlCommand())
+            using (SqlCommand com = new SqlCommand())
             {
                 com.Connection = con;
                 com.CommandText = "select * from student";
@@ -46,7 +46,57 @@ namespace Cwiczenia_03.Controllers
             }
         }
 
-      
+        [HttpGet("{indexNum}")]
+        public IActionResult GetStudent(string indexNum)
+        {
+            string conStrin = "Data Source=db-mssql;Initial Catalog=s17180;Integrated Security=True";
+            var info = new List<string>();
+
+
+            string studentsWithInfo = "select * from enrollment e join student s on e.idenrollment = s.idenrollment where s.IndexNumber = @index;";
+            string[] cols = { "idenrollment", "semester", "idstudy", "startdate" };
+            Dictionary<string, string> coulmnsNames = new Dictionary<string, string>();
+            coulmnsNames.Add("index", indexNum);
+
+
+            using (SqlConnection con = new SqlConnection(conStrin))
+            using (SqlCommand com = new SqlCommand())
+            {
+
+                com.Connection = con;
+                com.CommandText = (studentsWithInfo);
+
+
+                if (coulmnsNames == null)
+                {
+                    return NotFound();
+                }
+
+               
+                    foreach (KeyValuePair<string, string> param in coulmnsNames)
+                        com.Parameters.AddWithValue(param.Key, param.Value);
+
+          
+
+                con.Open();
+                SqlDataReader reader = com.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    foreach (string col in cols)
+                        info.Add("'" + col + "'=" + reader[col].ToString());
+                }
+
+                if (info.Count == 0)
+                    return NotFound();
+
+                return Ok(info);
+
+            }
+        }
+
+
         [HttpPost]
         public IActionResult CreateStudent(Student student)
         {
